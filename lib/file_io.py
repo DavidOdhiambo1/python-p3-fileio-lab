5eaba3ce919137b4a8093ccf2ab654ea0df32797
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2025-01-14 09:41:24 +0300

    first commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as Myfile:
+        Myfile.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as Myfile:
+        Myfile.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as Myfile:
+        return Myfile.read()
